877afa095dfc941134fd00d97d2b2d2c287aaef9
Emerald Hedgehog <s.boergers@live.de>
2023-02-25 22:22:31 +0100

    minor

--- pages/dashboard/my-profile.vue
+++ pages/dashboard/my-profile.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="relative">
+    <LayoutPageHeader headline="Mein Profil"> </LayoutPageHeader>
     <GenericContainerModal
       class="w-[340px] absolute top-12 left-1/2 -translate-x-1/2 text-xl text-center"
     >
