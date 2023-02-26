aaa5c59d0ac84f8c1e5a116977c37062aa5a57b6
Emerald Hedgehog <s.boergers@live.de>
2023-02-26 04:47:54 +0100

    added page-loader

--- app.vue
+++ app.vue
@@ -1,9 +1,20 @@
 <template>
   <NuxtLayout>
+    <LoaderPageDefault v-show="loading" />
     <NuxtLoadingIndicator />
     <NuxtPage class="grow h-full" />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+const loading = ref(false);
+nuxtApp.hook("page:start", () => {
+  loading.value = true;
+});
+nuxtApp.hook("page:finish", () => {
+  loading.value = false;
+});
+</script>
 <style lang="postcss">
 body {
   @apply font-texty;
---
+++ components/loader/page-default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fixed left-0 top-0 h-full w-full z-50 bg-black bg-opacity-75">
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div style="font-size: 98px" class="loader">🤔</div>
+    </div>
+  </div>
+</template>
+<style scoped lang="postcss">
+.loader {
+  text-shadow: 0 0 5px #000000, 0 0 10px #cd2cd3, 0 0 15px #ffffff,
+    0 0 20px #0b07ff;
+  animation: loaderwobble 1s linear infinite;
+}
+
+@keyframes loaderwobble {
+  0% {
+    scale: 1;
+  }
+  50% {
+    scale: 1.1;
+    rotate: 3deg;
+  }
+  100% {
+    scale: 1;
+  }
+}
+</style>
